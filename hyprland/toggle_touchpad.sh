2215503ff7900e2ee6735a5fbb9d1b9ea3993c0c
DaniD3v <sch220233@spengergasse.at>
2024-01-16 01:14:12 +0100

    made script smaller

--- hyprland/toggle_touchpad.sh
+++ hyprland/toggle_touchpad.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-# Fetch current state and toggle it
-current_state=$(hyprctl getoption "device:asue120a:00-04f3:319b-touchpad:enabled" | grep -oP "(?<=int: )\N")
-new_state=$((($current_state + 1) % 2))
+current_state=$(hyprctl getoption "device:asue120a:00-04f3:319b-touchpad:enabled" | grep -oP "(?<=int: )\N") # fetch old value
+new_state=$((($current_state + 1) % 2)) # reverse value
 
-echo $current_state
-echo $new_state
-
-# Set the new state
-hyprctl keyword "device:asue120a:00-04f3:319b-touchpad:enabled" $new_state
+hyprctl keyword "device:asue120a:00-04f3:319b-touchpad:enabled" $new_state # Set the new state
