152e1768be3bf1d72a19aa366ba05548a9c9f199
DaniD3v <sch220233@spengergasse.at>
2025-10-09 08:30:52 +0200

    container setup

--- src/host/server/default.nix
+++ src/host/server/default.nix
@@ -5,6 +5,39 @@
     ./disko.nix
   ];
 
+  virtualisation.arion = {
+    backend = "podman-socket";
+
+    projects = {
+      nginx.settings = {
+        project.name = "nginx";
+
+        services.nginx.service = {
+          image = "nginx";
+          networks = [ "nginx-reverse" ];
+        };
+
+        networks.nginx-reverse = { };
+      };
+
+      jellyfin.settings = {
+        project.name = "jellyfin";
+
+        services.jellyfin.service = {
+          image = "jellyfin/jellyfin";
+          networks = [ "nginx-reverse" ];
+
+          ports = [
+            # "8096:8096/tcp"
+            # "7359:7359/udp"
+          ];
+        };
+
+        networks.nginx-reverse.external = true;
+      };
+    };
+  };
+
   # Remove 32G swap for VMs
   virtualisation.vmVariantWithDisko = {
     disko.devices.disk.nvme.content.partitions.swap.size = lib.mkForce "512M";
