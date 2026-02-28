607d09cc7f484039a30f9550d529f32391968bf3
DaniD3v <sch220233@spengergasse.at>
2026-02-28 21:05:14 +0100

    fix(host thinkpad-e14): stable dns in k3s

--- src/host/thinkpad-e14/default.nix
+++ src/host/thinkpad-e14/default.nix
@@ -37,10 +37,21 @@
     };
   };
 
-  networking.extraHosts = ''
-    127.0.0.1 gitea.pse-cicd
-    127.0.0.1 jenkins.pse-cicd
-  '';
+  networking = {
+    # These make sure k3s has a static dns server
+    # HACK
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+    ];
+
+    extraHosts = ''
+      127.0.0.1 gitea.pse-cicd
+      127.0.0.1 jenkins.pse-cicd
+    '';
+  };
 
   # enable podman on user accounts
   virtualisation = {
