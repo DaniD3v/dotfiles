5d3bbbb7de50c36a510dffd6d3097e221703b7c1
DaniD3v <sch220233@spengergasse.at>
2026-03-26 12:47:12 +0100

    feat(user notyou): resolve netbird peers with resolved

--- src/host/thinkpad-e14/netbird.nix
+++ src/host/thinkpad-e14/netbird.nix
@@ -1,6 +1,11 @@
 {
-  services.netbird = {
-    clients."vpn-srino".port = 51820;
-    ui.enable = false;
+  services = {
+    netbird = {
+      clients."vpn-srino".port = 51820;
+      ui.enable = false;
+    };
+
+    # use resolvd => resolve peers with hostnames.netbird.cloud
+    resolved.enable = true;
   };
 }
